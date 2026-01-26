7e43e847d2fad068dbe526050de39dfb8c3b2110
nickcrabtree <nickcrabtree@gmail.com>
2026-01-26 21:33:22 +0000

    lint fixes

--- win/rfb_win32/WMWindowCopyRect.h
+++ win/rfb_win32/WMWindowCopyRect.h
@@ -1,15 +1,15 @@
 /* Copyright (C) 2002-2005 RealVNC Ltd.  All Rights Reserved.
- * 
+ *
  * This is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this software; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307,
@@ -31,23 +31,23 @@
 
 namespace rfb {
 
-  namespace win32 {
+namespace win32 {
 
-    class WMCopyRect {
-    public:
-      WMCopyRect();
+class WMCopyRect {
+public:
+  WMCopyRect();
 
-      bool processEvent();
-      bool setUpdateTracker(UpdateTracker* ut);
+  bool processEvent();
+  bool setUpdateTracker(UpdateTracker* ut);
 
-    protected:
-      UpdateTracker* ut;
-      void* fg_window;
-      core::Rect fg_window_rect;
-    };
-
-  };
-
+protected:
+  UpdateTracker* ut;
+  void* fg_window;
+  core::Rect fg_window_rect;
 };
+
+}; // namespace win32
+
+}; // namespace rfb
 
 #endif // __RFB_WIN32_WM_WINDOW_COPYRECT_H__
--- win/vncconfig/vncconfig.cxx
+++ win/vncconfig/vncconfig.cxx
@@ -1,34 +1,34 @@
 /* Copyright (C) 2002-2005 RealVNC Ltd.  All Rights Reserved.
- * 
+ *
  * This is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this software; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307,
  * USA.
  */
 
-#include <winsock2.h>
-#include <windows.h>
 #include <commctrl.h>
 #include <string.h>
+#include <windows.h>
+#include <winsock2.h>
 
 #include "resource.h"
 
-#include <core/Logger_stdio.h>
 #include <core/LogWriter.h>
+#include <core/Logger_stdio.h>
 
+#include <rfb_win32/CurrentUser.h>
 #include <rfb_win32/Dialog.h>
 #include <rfb_win32/RegConfig.h>
-#include <rfb_win32/CurrentUser.h>
 
 using namespace core;
 using namespace rfb;
@@ -36,18 +36,15 @@ using namespace rfb::win32;
 
 static LogWriter vlog("main");
 
-
 #include <vncconfig/Authentication.h>
 #include <vncconfig/Connections.h>
-#include <vncconfig/Sharing.h>
+#include <vncconfig/Desktop.h>
 #include <vncconfig/Hooking.h>
 #include <vncconfig/Inputs.h>
 #include <vncconfig/Legacy.h>
-#include <vncconfig/Desktop.h>
-
+#include <vncconfig/Sharing.h>
 
 const char* rfb::win32::AppName = "TigerVNC Configuration";
-
 
 #ifdef _DEBUG
 BoolParameter captureDialogs("CaptureDialogs", "", false);
@@ -55,10 +52,8 @@ BoolParameter captureDialogs("CaptureDialogs", "", false);
 
 HKEY configKey = HKEY_CURRENT_USER;
 
-
-void
-processParams(int argc, char* argv[]) {
-  for (int i=1; i<argc; i++) {
+void processParams(int argc, char* argv[]) {
+  for (int i = 1; i < argc; i++) {
     if (strcasecmp(argv[i], "-service") == 0) {
       configKey = HKEY_LOCAL_MACHINE;
     } else if (strcasecmp(argv[i], "-user") == 0) {
@@ -75,15 +70,14 @@ processParams(int argc, char* argv[]) {
   }
 }
 
-
 int WINAPI WinMain(HINSTANCE inst, HINSTANCE /*prev*/, char* /*cmdLine*/, int /*cmdShow*/) {
 
   // Configure debugging output
 #ifdef _DEBUG
   AllocConsole();
-  freopen("CONIN$","rb",stdin);
-  freopen("CONOUT$","wb",stdout);
-  freopen("CONOUT$","wb",stderr);
+  freopen("CONIN$", "rb", stdin);
+  freopen("CONOUT$", "wb", stdout);
+  freopen("CONOUT$", "wb", stderr);
   setbuf(stderr, nullptr);
   initStdIOLoggers();
   logParams.setParam("*:stderr:100");
@@ -107,7 +101,7 @@ int WINAPI WinMain(HINSTANCE inst, HINSTANCE /*prev*/, char* /*cmdLine*/, int /*
       // Create the required configuration registry key
       RegKey rootKey;
       rootKey.createKey(configKey, "Software\\TigerVNC\\WinVNC4");
-  
+
       // Override whatever security it already had (NT only)
       bool warnOnChangePassword = false;
       try {
@@ -127,10 +121,11 @@ int WINAPI WinMain(HINSTANCE inst, HINSTANCE /*prev*/, char* /*cmdLine*/, int /*
         // Something weird happens on NT 4.0 SP5 but I can't reproduce it on other
         // NT 4.0 service pack revisions.
         if (e.err == ERROR_INVALID_PARAMETER) {
-          MsgBox(nullptr, "Windows reported an error trying to secure the VNC server settings for this user.  "
-                    "Your settings may not be secure!", MB_ICONWARNING | MB_OK);
-        } else if (e.err != ERROR_CALL_NOT_IMPLEMENTED &&
-                   e.err != ERROR_NOT_LOGGED_ON) {
+          MsgBox(nullptr,
+                 "Windows reported an error trying to secure the VNC server settings for this user.  "
+                 "Your settings may not be secure!",
+                 MB_ICONWARNING | MB_OK);
+        } else if (e.err != ERROR_CALL_NOT_IMPLEMENTED && e.err != ERROR_NOT_LOGGED_ON) {
           // If the call is not implemented, ignore the error and continue
           throw;
         }
@@ -143,14 +138,21 @@ int WINAPI WinMain(HINSTANCE inst, HINSTANCE /*prev*/, char* /*cmdLine*/, int /*
 
       // Build the dialog
       std::list<PropSheetPage*> pages;
-      SecPage auth(rootKey); pages.push_back(&auth);
+      SecPage auth(rootKey);
+      pages.push_back(&auth);
       auth.setWarnPasswdInsecure(warnOnChangePassword);
-      ConnectionsPage conn(rootKey); pages.push_back(&conn);
-      InputsPage inputs(rootKey); pages.push_back(&inputs);
-      SharingPage sharing(rootKey); pages.push_back(&sharing);
-      DesktopPage desktop(rootKey); pages.push_back(&desktop);
-      HookingPage hooks(rootKey); pages.push_back(&hooks);
-      LegacyPage legacy(rootKey, configKey == HKEY_CURRENT_USER); pages.push_back(&legacy);
+      ConnectionsPage conn(rootKey);
+      pages.push_back(&conn);
+      InputsPage inputs(rootKey);
+      pages.push_back(&inputs);
+      SharingPage sharing(rootKey);
+      pages.push_back(&sharing);
+      DesktopPage desktop(rootKey);
+      pages.push_back(&desktop);
+      HookingPage hooks(rootKey);
+      pages.push_back(&hooks);
+      LegacyPage legacy(rootKey, configKey == HKEY_CURRENT_USER);
+      pages.push_back(&legacy);
 
       // Load the default icon to use
       HICON icon = (HICON)LoadImage(inst, MAKEINTRESOURCE(IDI_ICON), IMAGE_ICON, 0, 0, LR_SHARED);
